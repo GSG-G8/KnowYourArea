bdf55f14445650d95a8175e39e4e9ef9f8c49757
mu7ammadabed <mu7ammadabed@gmail.com>
2020-02-26 16:20:31 +0200

    Fix Errors

--- server/database/config/build.sql
+++ server/database/config/build.sql
@@ -8,19 +8,19 @@ CREATE TABLE locations (
     category varchar(100) NOT NULL,
     description varchar(255),
     coordinates varchar(100)
-)
+);
 
 CREATE TABLE nearby (
     id serial PRIMARY KEY,
     location_id integer REFERENCES locations(id) ON UPDATE CASCADE,
-    nearby_location varchar(100) NOT NULL,
-)
+    nearby_location varchar(100) NOT NULL
+);
 
 INSERT INTO locations(name, category, description, coordinates) VALUES
 ('Gaza Sky Geeks', 'Workspace', 'A place where you can work freely', '31.5129811,34.4464425'),
 ('IUG', 'University', 'Islamic University of Gaza', '31.5129811,34.4464425'),
 ('AUG', 'University', 'Al-Azhar University of Gaza', '31.5129811,34.4464425'),
-('Break', 'Restaurent', 'Break restaurent for food and drinks', '31.5129811,34.4464425')
+('Break', 'Restaurent', 'Break restaurent for food and drinks', '31.5129811,34.4464425');
 
 INSERT INTO nearby(location_id, nearby_location) VALUES
 (1,2),
@@ -28,6 +28,6 @@ INSERT INTO nearby(location_id, nearby_location) VALUES
 (1,4),
 (2,3),
 (2,4),
-(3,4)
+(3,4);
 
 COMMIT;
