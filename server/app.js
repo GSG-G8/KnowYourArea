1bd2eef3af0b542018f873e56a13e20a1b4296a5
rawan <rawan.designer@hotmail.com>
2020-02-26 14:24:32 +0200

    Handle public route Relates #5

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const path = require('path');
+
 
 const app = express();
 
 app.disable('x-powered-by');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '..','public')));
+
 
 app.set('port', process.env.PORT || 5000);
 
