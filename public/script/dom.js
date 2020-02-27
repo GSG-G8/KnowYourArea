3a22a7b984eae44f8fa6d63912a2c233f452cc5d
mu7ammadabed <mu7ammadabed@gmail.com>
2020-02-27 15:53:22 +0200

    Refactor the dom function Relates #25

--- public/script/dom.js
+++ public/script/dom.js
@@ -1,6 +1,10 @@
 const viewDetails = document.querySelector('#viewDetails');
 const searchInput = document.querySelector('#searchInput');
 
+const deleteNodeChilds = (node) => {
+  while (node.firstChild) node.removeChild(node.firstChild);
+};
+
 // Extra coordinates for testing (41.40338, 2.17403)
 let mapSrc = '31.5129811,34.4464425';
 const src = `https://www.google.com/maps/embed/v1/place?q=${mapSrc.split(',')[0]}%2C%20${mapSrc.split(',')[1]}&key=AIzaSyDDOHAK66Eu_kt42uNaSrmXoWZv8r3d_X8`;
@@ -17,26 +21,19 @@ document.querySelector('.header').appendChild(iframe);
 
 
 const renderTheData = (location) => {
-  console.log(location);
-  const section = document.querySelector('.results__container');
+  const header = document.querySelector('.results__container');
   const div = document.createElement('div');
-  const resultsName = document.createElement('h3');
-  resultsName.textContent = `Name: ${location[0].name}`;
-  div.appendChild(resultsName);
-
-  const resultCategory = document.createElement('h3');
-  resultCategory.textContent = `Category: ${location[0].category}`;
-  div.appendChild(resultCategory);
-
-  const resultDescription = document.createElement('h3');
-  resultDescription.textContent = `Description: ${location[0].description}`;
-  div.appendChild(resultDescription);
-
-  const resultCoordinates = document.createElement('h3');
-  resultCoordinates.textContent = `Coordinates: ${location[0].coordinates}`;
-  div.appendChild(resultCoordinates);
-
-  section.appendChild(div);
+  deleteNodeChilds(header);
+  const createElements = (description, param2) => {
+    const resultsName = document.createElement('h4');
+    resultsName.textContent = `${description}: ${location[0][param2]}`;
+    div.appendChild(resultsName);
+  };
+  createElements('Name', 'name');
+  createElements('Category', 'category');
+  createElements('Description', 'description');
+  createElements('Coordinates', 'coordinates');
+  header.appendChild(div);
 };
 
 viewDetails.addEventListener('click', () => {
