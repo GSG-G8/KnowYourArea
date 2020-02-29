e15d1b6b723933071e18d0f197d18db2c9253179
mu7ammadabed <mu7ammadabed@gmail.com>
2020-02-29 09:45:36 +0200

    Modify Dom and style

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,15 +1,22 @@
 const router = require('express').Router();
-const getData = require('../database/queries/getData');
+const { getData } = require('../database/queries/getData');
+const { getAllData } = require('../database/queries/getData');
 const addLocation = require('../database/queries/postData');
 
+router.get('/locations', (req, res) => {
+  getAllData()
+    .then(({ rows }) => res.json(rows))
+    .catch((err) => console.error(err));
+});
+
+router.get('/Slocations', (req, res) => {
+  getData(req.query.location)
+    .then(({ rows }) => res.json(rows))
+    .catch((err) => console.error(err));
+});
 
 router.post('/locations', (req, res) => {
   addLocation(req.body).then(() => res.redirect('/')).catch(console.error);
 });
 
-router.get('/locations', (req, res) => {
-  getData(req.query.location)
-    .then(({ rows }) => res.json(rows))
-    .catch((err) => console.error(err));
-});
 module.exports = router;
--- server/database/queries/getData.js
+++ server/database/queries/getData.js
@@ -1,5 +1,9 @@
 const connection = require('../config/connection');
 
 const getData = (location = 'gaza') => connection.query('select * from locations where name ilike $1', [`%${location}%`]);
+const getAllData = () => connection.query('select * from locations');
 
-module.exports = getData;
+module.exports = {
+  getData,
+  getAllData,
+};
